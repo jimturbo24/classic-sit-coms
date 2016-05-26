e5bb65d35283eaf0a6c9966c8f7e3e7260bec223
James Trbovich <jim.trbovich@gmail.com>
2016-05-25 23:55:19 -0500

    added bootstrap to the project

--- tv_comedies/shows/urls.py
+++ tv_comedies/shows/urls.py
@@ -1,28 +1,29 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'shows'
 urlpatterns = [
     url(r'^shows/$', views.show_index, name="show-index" ),
-    url(r'^shows/(?P<show_id>[0-9]+)/$', views.show_view, name='content-view_show'),
+    url(r'^shows/(?P<show_id>[0-9]+)/$', views.show_view, name='show-details'),
 
     url(r'^actors/$', views.actor_index, name="actor-index" ),
-    url(r'^actors/(?P<actor_id>[0-9]+)/$', views.actor_view, name='content-view_actor'),
+    url(r'^actors/(?P<actor_id>[0-9]+)/$', views.actor_view, name='actor-details'),
 
     url(r'^episodes/$', views.episode_index, name="episode-index" ),
-    url(r'^episodes/(?P<episode_id>[0-9]+)/$', views.episode_view, name='content-view_episode'),
+    url(r'^episodes/(?P<episode_id>[0-9]+)/$', views.episode_view, name='episode-details'),
 
     url(r'^seasons/$', views.season_index, name="season-index" ),
-    url(r'^seasons/(?P<season_id>[0-9]+)/$', views.season_view, name='content-view_season'),
+    url(r'^seasons/(?P<season_id>[0-9]+)/$', views.season_view, name='season-details'),
 
     url(r'^characters/$', views.character_index, name="character-index" ),
-    url(r'^characters/(?P<character_id>[0-9]+)/$', views.character_view, name='content-view_character'),
+    url(r'^characters/(?P<character_id>[0-9]+)/$', views.character_view, name='character-details'),
 
     url(r'^directors/$', views.director_index, name="director-index" ),
-    url(r'^directors/(?P<director_id>[0-9]+)/$', views.director_view, name='content-view_director'),
+    url(r'^directors/(?P<director_id>[0-9]+)/$', views.director_view, name='director-details'),
 
     url(r'^reviews/$', views.review_index, name="review-index" ),
-    url(r'^reviews/(?P<review_id>[0-9]+)/$', views.review_view, name='content-view_review'),
-    
+    url(r'^reviews/(?P<review_id>[0-9]+)/$', views.review_view, name='review-details'),
+
     url(r'^writers/$', views.writer_index, name="writer-index" ),
-    url(r'^writers/(?P<writer_id>[0-9]+)/$', views.writer_view, name='content-view_writer')
+    url(r'^writers/(?P<writer_id>[0-9]+)/$', views.writer_view, name='writer-details')
 ]
